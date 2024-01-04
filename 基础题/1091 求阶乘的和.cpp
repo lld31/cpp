34571981acbc04fe9d86eb4396ca32688f9c5382
lld31 <my@3jc.com>
2024-01-05 00:09:20 +0800

    更新备注

--- 基础题/1091 求阶乘的和.cpp
+++ 基础题/1091 求阶乘的和.cpp
@@ -1,18 +1,20 @@
 #include <stdio.h>
 /*
-	给定正整数n，求不大于n的正整数的阶乘的和（即求1!+2!+3!+...+n!），输出阶乘的和。。
+	给定正整数n，求不大于n的正整数的阶乘的和（即求1!+2!+3!+...+n!），输出阶乘的和。
+	【输入】输入有一行，包含一个正整数n（1 < n < 12）。
+	【输出】输出有一行：阶乘的和。
 */
 int main() {
-	int n, count = 1, sum = 0, j = 1;
+	int n, count = 1, sum = 0;
 	printf("请输入一个正整数：");
 	scanf("%d", &n);
 
 	while (n >= count) {
+		int j = 1; // 重置j为1
 		for (int i = 1; i <= count; i++) {
 			j *= i;
 		}
 		sum += j;
-		printf("%d %d %d\n", count, j, sum);
 		count++;
 	}
 
